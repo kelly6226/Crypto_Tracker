f0a75ef086cc562197406ffda2f177f2e3e31b15
Jisoo <kelly6226@naver.com>
2023-10-09 22:08:57 +0900

    [FEAT] #5.0 Setup - Route 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
+import Router from './Router';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return  <Router />;
 }
 
 export default App;
