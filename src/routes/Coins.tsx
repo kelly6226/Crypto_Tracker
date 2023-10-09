aa757b2f7eae046cafc2c9ab05c6965d66823aac
Jisoo <kelly6226@naver.com>
2023-10-09 23:40:58 +0900

    [FEAT] coinpaprika API 연동 후 data 확인

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import {useState, useEffect} from 'react';
 
 const Container = styled.div`
     padding: 0px 20px;
+    max-width: 480px;
+    margin: 0 auto;
 `;
 
 const Header = styled.header`
@@ -36,36 +39,27 @@ const Title = styled.h1`
     color: ${props => props.theme.accentColor};
 `;
 
+interface CoinInterface {
+    id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    is_new: boolean,
+    is_active: boolean,
+    type: string,
+}
+
+
 function Coins() {
-    const coins = [
-        {
-            id: "btc-bitcoin",
-            name: "Bitcoin",
-            symbol: "BTC",
-            rank: 1,
-            is_new: false,
-            is_active: true,
-            type: "coin",
-            },
-            {
-            id: "eth-ethereum",
-            name: "Ethereum",
-            symbol: "ETH",
-            rank: 2,
-            is_new: false,
-            is_active: true,
-            type: "coin",
-            },
-            {
-            id: "hex-hex",
-            name: "HEX",
-            symbol: "HEX",
-            rank: 3,
-            is_new: false,
-            is_active: true,
-            type: "token",
-            },
-    ]
+    const [coins, setCoins] = useState<CoinInterface[]>([]);
+    useEffect(() => {
+        (async() => {
+            const response = await fetch("https://api.coinpaprika.com/v1/coins")
+            const json = await response.json();
+            console.log(json);
+        })();
+    }, [])
+
 
     return (
         <Container>
